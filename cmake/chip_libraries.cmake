11e1ec973d5017985552ecfd347c8ee2af193dac
S de Wit <dev@sjorsdewit.name>
2018-05-04 14:31:17 +0200

    update packages some more

--- cmake/chip_libraries.cmake
+++ cmake/chip_libraries.cmake
@@ -15,21 +15,24 @@ if("${MCU_PLATFORM}" STREQUAL "43xx_m4")
 
     CPM_AddModule("lpc_chip_43xx_m4"
         GIT_REPOSITORY "https://github.com/JitterCompany/lpc_chip_43xx_m4.git"
-        GIT_TAG "3.1")
+        GIT_TAG "3.2"
+        USE_EXISTING_VER TRUE)
 
 elseif("${MCU_PLATFORM}" STREQUAL "43xx_m0")
     message(STATUS "${CPM_MODULE_NAME}: Platform '43xx_m0' detected")
 
     CPM_AddModule("lpc_chip_43xx_m0"
         GIT_REPOSITORY "https://github.com/JitterCompany/lpc_chip_43xx_m0.git"
-        GIT_TAG "3.1.1")
+        GIT_TAG "3.2"
+        USE_EXISTING_VER TRUE)
 
 elseif("${MCU_PLATFORM}" STREQUAL "11uxx")
     message(STATUS "${CPM_MODULE_NAME}: Platform '11uxx' detected")
 
     CPM_AddModule("lpc_chip_11uxx"
         GIT_REPOSITORY "https://github.com/JitterCompany/lpc_chip_11uxx.git"
-        GIT_TAG "1.0")
+        GIT_TAG "1.0"
+        USE_EXISTING_VER TRUE)
 
 else()
     message(FATAL_ERROR "${CPM_MODULE_NAME}: platform '${MCU_PLATFORM}' not supported")
