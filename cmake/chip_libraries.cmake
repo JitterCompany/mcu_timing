dfaef450efc14be813d630718368822527965a8b
S de Wit <dev@sjorsdewit.name>
2018-08-28 18:40:21 +0200

    update chip library naming conventions

--- cmake/chip_libraries.cmake
+++ cmake/chip_libraries.cmake
@@ -13,26 +13,25 @@ endif()
 if("${MCU_PLATFORM}" STREQUAL "43xx_m4")
     message(STATUS "${CPM_MODULE_NAME}: Platform '43xx_m4' detected")
 
-    CPM_AddModule("lpc_chip_43xx_m4"
-        GIT_REPOSITORY "https://github.com/JitterCompany/lpc_chip_43xx_m4.git"
+    CPM_AddModule("chip_lpc43xx_m4"
+        GIT_REPOSITORY "https://github.com/JitterCompany/chip_lpc43xx_m4.git"
         GIT_TAG "3.2"
         USE_EXISTING_VER TRUE)
 
 elseif("${MCU_PLATFORM}" STREQUAL "43xx_m0")
     message(STATUS "${CPM_MODULE_NAME}: Platform '43xx_m0' detected")
 
-    CPM_AddModule("lpc_chip_43xx_m0"
-        GIT_REPOSITORY "https://github.com/JitterCompany/lpc_chip_43xx_m0.git"
+    CPM_AddModule("chip_lpc43xx_m0"
+        GIT_REPOSITORY "https://github.com/JitterCompany/chip_lpc43xx_m0.git"
         GIT_TAG "3.2"
         USE_EXISTING_VER TRUE)
 
 elseif("${MCU_PLATFORM}" STREQUAL "11uxx")
     message(STATUS "${CPM_MODULE_NAME}: Platform '11uxx' detected")
 
-    CPM_AddModule("lpc_chip_11uxx"
-        GIT_REPOSITORY "https://github.com/JitterCompany/lpc_chip_11uxx.git"
-        GIT_TAG "1.0"
-        USE_EXISTING_VER TRUE)
+    CPM_AddModule("chip_lpc11uxx"
+        GIT_REPOSITORY "https://github.com/JitterCompany/chip_lpc11uxx.git"
+        GIT_TAG "1.1")
 
 else()
     message(FATAL_ERROR "${CPM_MODULE_NAME}: platform '${MCU_PLATFORM}' not supported")
